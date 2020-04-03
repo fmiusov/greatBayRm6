41d645248c6a7f2e0e04ce52243b8968753255cd
Eshi44 <e.hameliervolino@gmail.com>
2020-04-03 13:21:13 -0400

    edited sql file

--- greatBay.sql
+++ greatBay.sql
@@ -5,6 +5,9 @@ USE greatBay_bd;
 
     CREATE  TABLE  auction_off(
         id INT NOT NULL AUTO_INCREMENT,
-        name VARCHAR(50) NOT NULL,
+        name_of_item VARCHAR(50) NOT NULL,
+        section VARCHAR(50) NOT NULL,
+        start_bid INT DEFUALT 0,
+        highest_bid INT DEFUALT 0,
         PRIMARY KEY(id)  
     );
